53e8d40274e5904a8bd84afd3d19cd2d3d441262
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-31 22:19:41 +0530

    ansible practice - variables

--- run-scripts.sh
+++ run-scripts.sh
@@ -2,6 +2,4 @@ if [ -z "$1" ]; then
   echo "component name is missing"
   exit 1
 fi
-ansible-playbook -i $1-dev.devopsbymanju.shop, \
--e ansible_user=ec2-user -e ansible_password=DevOps321 \
--e component_name=$1 roboshop.yaml
+ansible-playbook -i $1-dev.devopsbymanju.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component_name=$1 roboshop.yaml
